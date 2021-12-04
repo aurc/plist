b5965b06695164469aaf544577d6ad2dc289153e
aurc <54873456+aurc@users.noreply.github.com>
2021-12-04 17:41:40 +1100

    Small improvements

--- jsonparser.go
+++ jsonparser.go
@@ -67,7 +67,7 @@ func Convert(in []byte, config *Config) ([]byte, error) {
 	root := doc.Root
 	var output string
 	if config.HighFidelity {
-		output = toJsonFill(root.FirstChild(), nil)
+		output = toJsonFill(root.FirstChild())
 	} else {
 		itm, _ := makePLTypeFromNode(root.FirstChild())
 		output = itm.toJson(nil)
@@ -130,50 +130,39 @@ type plnode struct {
 	value  interface{}
 }
 
-func toJsonFill(n, parent *xmldom.Node) string {
+func toJsonFill(n *xmldom.Node) string {
 	v := ""
-	if n == nil {
-		return v
-	}
 	switch n.Name {
 	case "key":
 		v += fmt.Sprintf("{\"k\":\"%s\",", n.Text)
 		next := n.NextSibling()
-		v += fmt.Sprintf("\"v\":%s}", toJsonFill(next, nil))
-		return v
+		v += fmt.Sprintf("\"v\":%s}", toJsonFill(next))
+
 	case "dict":
 		v += fmt.Sprintf("{\"type\":\"dict\",\"value\":[")
 		var arr []string
 		for i, cn := range n.Children {
 			if i%2 == 0 {
-				arr = append(arr, toJsonFill(cn, n))
+				arr = append(arr, toJsonFill(cn))
 			}
 		}
 		v += strings.Join(arr, ",") + "]}"
-		return v
 	case "true", "false":
 		v += fmt.Sprintf("{\"type\":\"bool\",\"value\":\"%s\"}", n.Name)
-		return v
 	case "string", "real", "integer", "date":
 		v += fmt.Sprintf("{\"type\":\"%s\",\"value\":\"%s\"}", n.Name, n.Text)
-		return v
 	case "data":
 		v += fmt.Sprintf("{\"type\":\"%s\",\"value\":%s}", n.Name, trimData(n.Text))
-		return v
 	case "array":
 		v += fmt.Sprintf("{\"type\":\"array\",\"value\":[")
 		var arr []string
 		for _, cn := range n.Children {
-			itm := toJsonFill(cn, n)
+			itm := toJsonFill(cn)
 			arr = append(arr, fmt.Sprintf("%s", itm))
 		}
 		v += strings.Join(arr, ",") + "]}"
-		//if parent == nil {
-		//	v = fmt.Sprintf("{%s}", v)
-		//}
-		return v
 	}
-	return ""
+	return v
 }
 
 func trimData(data interface{}) string {
@@ -235,12 +224,14 @@ func (node *plnode) toJson(parent *plnode) string {
 }
 
 func makePLTypeFromNode(n *xmldom.Node) (*plnode, *xmldom.Node) {
+	var pl *plnode
+	var ns *xmldom.Node
 	switch n.Name {
 	case "key":
 		next := n.NextSibling()
 		tp, _ := makePLTypeFromNode(next)
 		tp.name = n.Text
-		return makePLType(n.Text, tp.pltype, tp.value), next.NextSibling()
+		pl, ns = makePLType(n.Text, tp.pltype, tp.value), next.NextSibling()
 	case "dict":
 		var dArr []*plnode
 		for i, cn := range n.Children {
@@ -250,29 +241,29 @@ func makePLTypeFromNode(n *xmldom.Node) (*plnode, *xmldom.Node) {
 				dArr = append(dArr, itm)
 			}
 		}
-		return makePLType("", Dict, dArr), n.NextSibling()
+		pl, ns = makePLType("", Dict, dArr), n.NextSibling()
 	case "true":
-		return makePLType("Bool", Bool, true), nil
+		pl, ns = makePLType("Bool", Bool, true), nil
 	case "false":
-		return makePLType("Bool", Bool, false), nil
+		pl, ns = makePLType("Bool", Bool, false), nil
 	case "string":
-		return makePLType("String", String, n.Text), nil
+		pl, ns = makePLType("String", String, n.Text), nil
 	case "real":
 		v, err := strconv.ParseFloat(n.Text, 64)
 		if err != nil {
 			panic(err)
 		}
-		return makePLType("Real", Real, v), nil
+		pl, ns = makePLType("Real", Real, v), nil
 	case "integer":
 		v, err := strconv.ParseInt(n.Text, 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		return makePLType("Integer", Integer, v), nil
+		pl, ns = makePLType("Integer", Integer, v), nil
 	case "date":
-		return makePLType("Date", Date, n.Text), nil
+		pl, ns = makePLType("Date", Date, n.Text), nil
 	case "data":
-		return makePLType("Data", Data, n.Text), nil
+		pl, ns = makePLType("Data", Data, n.Text), nil
 	case "array":
 		var dArr []*plnode
 		for _, cn := range n.Children {
@@ -280,9 +271,9 @@ func makePLTypeFromNode(n *xmldom.Node) (*plnode, *xmldom.Node) {
 			itm, _ := makePLTypeFromNode(currChild)
 			dArr = append(dArr, itm)
 		}
-		return makePLType("array", Array, dArr), n.NextSibling()
+		pl, ns = makePLType("array", Array, dArr), n.NextSibling()
 	}
-	return nil, nil
+	return pl, ns
 }
 
 func makePLType(name string, pltype pltype, value interface{}) *plnode {
