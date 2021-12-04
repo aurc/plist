27bf9f091890282778a9ff5b6b2095adbcb66368
Aurelio Calegari <Aurelio.Calegari@anz.com>
2021-12-04 12:35:02 +1100

    fixed script bug

--- ci/tag_prod.sh
+++ ci/tag_prod.sh
@@ -18,5 +18,5 @@ CURV=$(git describe --tags --abbrev=0)
 IFS='.' read -ra VR <<< "$CURV"
 INC=`expr ${VR[2]} + 1`
 FV="${VR[0]}.${VR[1]}.$INC"
-git tag ${FV} && git push origin ${FV}
-GOPROXY=proxy.golang.org go list -m github.com/aurc/plist@${FV}
+git tag "${FV}" && git push origin "${FV}"
+GOPROXY=proxy.golang.org go list -m "github.com/aurc/plist@${FV}"
